ca99760cdc991a24ac1270bff2a1dd83fd190bd9
devopsabhijit <abhijitpimple09@gmail.com>
2026-01-28 17:44:27 +0530

    Standard print format on terminal

---
+++ day08/printformat.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+printf "%-5s %-10s %-5s \n" No Name Mark
+printf "%-5s %-10s %-4.2f \n" 1 Ben 90.05
+printf "%-5s %-10s %-4.2f \n" 2 Kevin 80.87
+printf "%-5s %-10s %-4.2f \n" 3 Jofra 85.05
+
